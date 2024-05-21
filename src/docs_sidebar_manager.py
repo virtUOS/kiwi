7acc1362eb1958f91e3a8aecae00fd9f2ff845b0
odrec <renatogarita@gmail.com>
2024-05-21 16:08:11 +0200

    More detailed helping text for chunk and overlap

--- src/docs_sidebar_manager.py
+++ src/docs_sidebar_manager.py
@@ -23,16 +23,31 @@ class SidebarDocsManager:
                 st.number_input(
                     label=session_state['_']("Input chunk size"),
                     min_value=100, max_value=4000, value=300, key='chunk_size',
-                    help=(session_state['_']("The chunk size specifies the amount of characters each "
-                          "chunk of text contains. Adjust based on the level of detail needed."))
+                    help=(
+                        session_state['_'](
+                            "The chunk size specifies the number of characters in each chunk of text. For example, "
+                            "with a chunk size of 300, the text will be divided into pieces of approximately 300 "
+                            "characters each. Adjust this based on the level of detail needed.\n\n"
+                            "**Why it's important**: A larger chunk size retains more context but might "
+                            "be less detailed. A smaller chunk size provides finer detail but may lose some "
+                            "context across chunks."
+                        )
+                    )
                 )
 
                 st.number_input(
                     label=session_state['_']("Overlap size"),
                     min_value=0, max_value=3000, value=20, key='overlap_size',
-                    help=session_state['_']("The overlap size specifies the character count that will overlap "
-                                            "between consecutive chunks. It helps in maintaining context "
-                                            "connectivity across chunks. Adjust based on the required continuity.")
+                    help=(
+                        session_state['_'](
+                            "The overlap size specifies the number of characters that will overlap between "
+                            "consecutive chunks. For example, with an overlap size of 20, the last 20 characters "
+                            "of one chunk will be the first 20 characters of the next chunk.\n\n**Why it's important**:"
+                            " The overlap helps maintain context between chunks. Without adequate overlap, "
+                            "some important details may be lost between chunks. Too much overlap "
+                            "can lead to redundant information."
+                        )
+                    )
                 )
 
 
